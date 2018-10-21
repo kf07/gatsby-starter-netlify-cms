4fe7b48f9c18cf7c1e6d926f8a7c4b2f1edff015
furusu <furusu0715@gmail.com>
2018-10-21 23:02:00 +0900

    ヘッダーのデザイン変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,5 +34,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
+    'gatsby-plugin-styled-components'
   ],
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,43 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import {Link} from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import styled from 'styled-components';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <AppBar position="static">
+      <Toolbar>
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              <Title>
+                備忘録
+              </Title>
+            </Link>
+            <div className="navbar-end">
+              <a
+                className="navbar-item"
+                href="https://github.com/kf07"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
           <span className="icon">
-            <img src={github} alt="Github" />
+            <img src={github} alt="Github"/>
           </span>
-        </a>
-      </div>
-    </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Toolbar>
+    </AppBar>
   </nav>
 )
+
+const Title = styled.div`
+  font-size: 28px;
+  color: #fff;
+`
 
 export default Navbar
